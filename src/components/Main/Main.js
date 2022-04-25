dfa90d1ec2277330b702e8e6be55644912699c2b
Sasha <al.pryadchenko@gmail.com>
2022-04-25 19:06:12 +0200

    reloaded files

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,16 +1,23 @@
 import React from "react";
 // import useTranslation from "../customHooks/translations";
 
-import "../../css/Header.css";
+import "../../css/Main.css";
 
 function Main() {
   // const strings = useTranslation();
 
   return (
     <main>
-      <ul>
-        <li></li>
-        <li></li>
+      <ul className="Main-content-list">
+        <li>
+          <h3 className="Main-headlines">Новини</h3>
+        </li>
+        <li>
+          <h3 className="Main-headlines">Оголошення</h3>
+        </li>
+        <li>
+          <h3 className="Main-headlines">Iнше</h3>
+        </li>
       </ul>
     </main>
   );
